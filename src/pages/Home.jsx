027d49d7cbefc03b1bfb79fbb3c3c336ee72ce0b
123Afroj123 <mulaniafroj6@gmail.com>
2025-11-06 00:21:46 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import {  Routes, Route } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { personalData } from "../utils/data/personal-data";
 import HeroSection from "../components/homepage/hero-section/HeroSection";
 import AboutSection from "../components/homepage/about/AboutSection";
 import Experience from "../components/homepage/experience/Experience";
@@ -8,11 +7,11 @@ import Projects from "../components/homepage/projects/Projects";
 import Education from "../components/homepage/education/Education";
 
 import ContactSection from "../components/homepage/contact/ContactSection";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import ScrollToTop from "../components/helper/scroll-to-top";
-import GlowCard from "../components/helper/glow-card";
+
 import Footer from "../components/footer";
-import Proj from "../components/homepage/projects/Proj";
+
 function Home() {
   return (
       
@@ -28,7 +27,7 @@ function Home() {
       <Projects />
       <Education />
       <ContactSection />
-      <Proj/>
+     
     </div>
     <Footer/>
    </>
