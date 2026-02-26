296df3e3cdee3bef45a645862b3d412cc64d5bde
Afroj Mulani <mulaniafroj26@gmail.com>
2026-02-26 19:51:15 +0530

    Fix email and update resume link in personal-data.js
    Updated email address and resume link in personal data.

--- src/utils/data/personal-data.js
+++ src/utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/am.jpg',
   designation: "Web Developer",
   description: "My name is Afroj Mulani.A Focused developer specializing in React and Node.js. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'mulaniafroj26@gmail.com',
+  email: 'mulaniafroj6@gmail.com',
   phone: '+917821016070',
   address: 'Latur , Maharashtra ',
   github: 'https://github.com/CodeByAfroj',
@@ -11,5 +11,5 @@ export const personalData = {
   twitter: 'https://x.com/afroj_mulani26',
   leetcode: "https://leetcode.com/u/Coder_Afroj_26",
   devUsername: "",
-  resume: "https://drive.google.com/"
+  resume: "https://drive.google.com/file/d/127I1JgX29NPayLpMRDBsbd4FSXKHCxOG/view?usp=sharing"
 }
